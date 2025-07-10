ca404569b154659754e29395e337d235c9f859b8
NZpatelK <patelk.dev@hotmail.com>
2025-07-10 19:12:21 +1200

    add new task fixed

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -18,16 +18,24 @@ interface CardProps {
 
 export default function Board() {
     const [cards, setCards] = useState<CardProps[]>(LIST_DATA);
-    const [columns, setColumns] = useState<ColumnProps[]>([
+    const [columns] = useState<ColumnProps[]>([
         { title: "Backlog", headingColor: "text-gray-200", column: "backlog" },
         { title: "To Do", headingColor: "text-yellow-200", column: "todo" },
         { title: "In Progress", headingColor: "text-blue-200", column: "doing" },
-        { title: "Done", headingColor: "text-emerald-200", column: "done" }]);
+        { title: "Done", headingColor: "text-emerald-200", column: "done" },
+    ]);
 
     return (
         <div className="h-full w-full flex gap-3">
             {columns.map((column, index) => (
-                <Column key={index} title={column.title} headingColor={column.headingColor} column={column.column} cards={cards} setCards={(e: string[]) => { setCards([...e]) }} />
+                <Column
+                    key={index}
+                    title={column.title}
+                    headingColor={column.headingColor}
+                    column={column.column}
+                    cards={cards}
+                    setCards={setCards} 
+                />
             ))}
             <BurnBarrel cards={cards} />
         </div>
