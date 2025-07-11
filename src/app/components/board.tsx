a99aaf840ec073bf7c98815a0ef2da88516c4aae
NZpatelK <patelk.dev@hotmail.com>
2025-07-11 17:29:02 +1200

    create function to allow user can add more column

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 import Column from "./Column";
 import { LIST_DATA } from "@/lib/data/listData";
 import BurnBarrel from "./BurnBarrel";
+import { FiPlus } from "react-icons/fi";
 
 interface ColumnProps {
     title: string;
@@ -18,12 +19,24 @@ interface CardProps {
 
 export default function Board() {
     const [cards, setCards] = useState<CardProps[]>(LIST_DATA);
-    const [columns] = useState<ColumnProps[]>([
+    const [columns, setColumns] = useState<ColumnProps[]>([
         { title: "Backlog", headingColor: "text-gray-200", column: "backlog" },
         { title: "To Do", headingColor: "text-yellow-200", column: "todo" },
         { title: "In Progress", headingColor: "text-blue-200", column: "doing" },
         { title: "Done", headingColor: "text-emerald-200", column: "done" },
     ]);
+
+    const handleCreateNewColumn = (e) => {
+        e.preventDefault();
+        const newColumn: ColumnProps = {
+            title: "Column " + (columns.length + 1),
+            headingColor: "text-gray-200",
+            column: "column" + (columns.length + 1),
+        };
+
+        setColumns([...columns, newColumn]);
+
+    }
 
     return (
         <div className="flex gap-3 m-20">
@@ -34,10 +47,15 @@ export default function Board() {
                     headingColor={column.headingColor}
                     column={column.column}
                     cards={cards}
-                    setCards={setCards} 
+                    setCards={setCards}
                 />
             ))}
-            <BurnBarrel setCards={setCards} />
+
+            <div className="">
+                <button onClick={handleCreateNewColumn} className="flex items-center text-xs text-nowrap text-gray-400  mt-1 mx-5 hover:text-gray-300"><span className="mr-2">Add Column</span> <FiPlus /></button>
+            </div>
+
+            {/* <BurnBarrel setCards={setCards} /> */}
         </div>
     );
 }
