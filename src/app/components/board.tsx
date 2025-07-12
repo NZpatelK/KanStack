c7e0d2abf63f0d8bb3ff633e18e122feb0964b7c
NZpatelK <patelk.dev@hotmail.com>
2025-07-12 16:27:49 +1200

    Update board.tsx

--- src/app/components/board.tsx
+++ src/app/components/board.tsx
@@ -64,7 +64,7 @@ export default function Board() {
             ))}
 
             <div className="">
-                <button onClick={handleCreateNewColumn} className="flex items-center text-xs text-nowrap text-gray-400  mt-1 mx-5 hover:text-gray-300"><span className="mr-2">Add Column</span> <FiPlus /></button>
+                <button onClick={handleCreateNewColumn} className="flex items-center p-2 text-xs text-nowrap text-gray-400  mt-1 mx-5 hover:text-gray-300"><span className="mr-2">Add Column</span> <FiPlus /></button>
             </div>
 
             {/* <BurnBarrel setCards={setCards} /> */}
